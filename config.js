95c9b448973530f3612cb2dd249e4475782d71da
carigothaya42-wq <carigothaya42@gmail.com>
2025-10-31 20:46:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐒𝐄𝐍𝐔-𝐌𝐃=lQY3HKZJ#KPpEjslCQtdbjiJ43_4yriNkNlAKj_OdZ10bdaPR6cc", // ඔයාගේ session id එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "𝐒𝐄𝐍𝐔-𝐌𝐃=DBkyTChD#jGlxCAU73MhCiQu8sZGJU6-9AuabyCWk8DASYjsXsoY", // ඔයාගේ session id එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb://mongo:cfWaBZIdEJVxOhUHDnXaGrmFZHkVgWsY@gondola.proxy.rlwy.net:51807", // ඔයාගේ mongodb url එක දාන්න
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/bjPrbF84/3174.jpg",
 BOT_NAME: process.env.BOT_NAME || "𝐒𝐄𝐍𝐔-𝐌𝐃",
